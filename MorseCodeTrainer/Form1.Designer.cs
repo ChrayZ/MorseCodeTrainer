20e19b37619a899a13da18b5d53e41b4c9f7d3e6
Chris <christianwenz2002@gmail.com>
2024-03-07 19:56:55 +0100

    Projektdateien hinzufügen.

---
+++ MorseCodeTrainer/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace MorseCodeTrainer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNextLetter = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lblHint = new System.Windows.Forms.Label();
+            this.lblStreak = new System.Windows.Forms.Label();
+            this.rbnLetterToMorse = new System.Windows.Forms.RadioButton();
+            this.rbnMorseToLetter = new System.Windows.Forms.RadioButton();
+            this.rbnMorseToLetterHearing = new System.Windows.Forms.RadioButton();
+            this.rbnMorseToWordHearing = new System.Windows.Forms.RadioButton();
+            this.nudMorsePitch = new System.Windows.Forms.NumericUpDown();
+            this.nudMorseInterval = new System.Windows.Forms.NumericUpDown();
+            this.lblMorseInterval = new System.Windows.Forms.Label();
+            this.lblMorsePitch = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMorsePitch)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMorseInterval)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblNextLetter
+            // 
+            this.lblNextLetter.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblNextLetter.AutoSize = true;
+            this.lblNextLetter.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNextLetter.Location = new System.Drawing.Point(400, 50);
+            this.lblNextLetter.Name = "lblNextLetter";
+            this.lblNextLetter.Size = new System.Drawing.Size(0, 46);
+            this.lblNextLetter.TabIndex = 0;
+            this.lblNextLetter.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.Location = new System.Drawing.Point(245, 199);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(311, 53);
+            this.txtInput.TabIndex = 1;
+            this.txtInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblHint
+            // 
+            this.lblHint.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblHint.AutoSize = true;
+            this.lblHint.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHint.Location = new System.Drawing.Point(400, 355);
+            this.lblHint.Name = "lblHint";
+            this.lblHint.Size = new System.Drawing.Size(0, 46);
+            this.lblHint.TabIndex = 2;
+            // 
+            // lblStreak
+            // 
+            this.lblStreak.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblStreak.AutoSize = true;
+            this.lblStreak.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStreak.Location = new System.Drawing.Point(80, 167);
+            this.lblStreak.Name = "lblStreak";
+            this.lblStreak.Size = new System.Drawing.Size(101, 62);
+            this.lblStreak.TabIndex = 3;
+            this.lblStreak.Text = "Streak:\r\n0\r\n";
+            this.lblStreak.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // rbnLetterToMorse
+            // 
+            this.rbnLetterToMorse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.rbnLetterToMorse.AutoSize = true;
+            this.rbnLetterToMorse.Location = new System.Drawing.Point(12, 352);
+            this.rbnLetterToMorse.Name = "rbnLetterToMorse";
+            this.rbnLetterToMorse.Size = new System.Drawing.Size(96, 17);
+            this.rbnLetterToMorse.TabIndex = 4;
+            this.rbnLetterToMorse.TabStop = true;
+            this.rbnLetterToMorse.Text = "Letter to Morse";
+            this.rbnLetterToMorse.UseVisualStyleBackColor = true;
+            // 
+            // rbnMorseToLetter
+            // 
+            this.rbnMorseToLetter.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.rbnMorseToLetter.AutoSize = true;
+            this.rbnMorseToLetter.Location = new System.Drawing.Point(12, 375);
+            this.rbnMorseToLetter.Name = "rbnMorseToLetter";
+            this.rbnMorseToLetter.Size = new System.Drawing.Size(93, 17);
+            this.rbnMorseToLetter.TabIndex = 5;
+            this.rbnMorseToLetter.TabStop = true;
+            this.rbnMorseToLetter.Text = "Morse to Leter";
+            this.rbnMorseToLetter.UseVisualStyleBackColor = true;
+            // 
+            // rbnMorseToLetterHearing
+            // 
+            this.rbnMorseToLetterHearing.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.rbnMorseToLetterHearing.AutoSize = true;
+            this.rbnMorseToLetterHearing.Location = new System.Drawing.Point(12, 398);
+            this.rbnMorseToLetterHearing.Name = "rbnMorseToLetterHearing";
+            this.rbnMorseToLetterHearing.Size = new System.Drawing.Size(125, 17);
+            this.rbnMorseToLetterHearing.TabIndex = 6;
+            this.rbnMorseToLetterHearing.TabStop = true;
+            this.rbnMorseToLetterHearing.Text = "Morse to letter (listen)";
+            this.rbnMorseToLetterHearing.UseVisualStyleBackColor = true;
+            // 
+            // rbnMorseToWordHearing
+            // 
+            this.rbnMorseToWordHearing.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.rbnMorseToWordHearing.AutoSize = true;
+            this.rbnMorseToWordHearing.Location = new System.Drawing.Point(12, 421);
+            this.rbnMorseToWordHearing.Name = "rbnMorseToWordHearing";
+            this.rbnMorseToWordHearing.Size = new System.Drawing.Size(128, 17);
+            this.rbnMorseToWordHearing.TabIndex = 7;
+            this.rbnMorseToWordHearing.TabStop = true;
+            this.rbnMorseToWordHearing.Text = "Letter to morse (listen)";
+            this.rbnMorseToWordHearing.UseVisualStyleBackColor = true;
+            // 
+            // nudMorsePitch
+            // 
+            this.nudMorsePitch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.nudMorsePitch.Location = new System.Drawing.Point(732, 421);
+            this.nudMorsePitch.Maximum = new decimal(new int[] {
+            16000,
+            0,
+            0,
+            0});
+            this.nudMorsePitch.Minimum = new decimal(new int[] {
+            37,
+            0,
+            0,
+            0});
+            this.nudMorsePitch.Name = "nudMorsePitch";
+            this.nudMorsePitch.Size = new System.Drawing.Size(56, 20);
+            this.nudMorsePitch.TabIndex = 8;
+            this.nudMorsePitch.Value = new decimal(new int[] {
+            800,
+            0,
+            0,
+            0});
+            // 
+            // nudMorseInterval
+            // 
+            this.nudMorseInterval.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.nudMorseInterval.Location = new System.Drawing.Point(732, 395);
+            this.nudMorseInterval.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.nudMorseInterval.Name = "nudMorseInterval";
+            this.nudMorseInterval.Size = new System.Drawing.Size(56, 20);
+            this.nudMorseInterval.TabIndex = 9;
+            this.nudMorseInterval.Value = new decimal(new int[] {
+            150,
+            0,
+            0,
+            0});
+            // 
+            // lblMorseInterval
+            // 
+            this.lblMorseInterval.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblMorseInterval.AutoSize = true;
+            this.lblMorseInterval.Location = new System.Drawing.Point(652, 397);
+            this.lblMorseInterval.Name = "lblMorseInterval";
+            this.lblMorseInterval.Size = new System.Drawing.Size(74, 13);
+            this.lblMorseInterval.TabIndex = 10;
+            this.lblMorseInterval.Text = "Morse-Interval";
+            // 
+            // lblMorsePitch
+            // 
+            this.lblMorsePitch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblMorsePitch.AutoSize = true;
+            this.lblMorsePitch.Location = new System.Drawing.Point(663, 423);
+            this.lblMorsePitch.Name = "lblMorsePitch";
+            this.lblMorsePitch.Size = new System.Drawing.Size(63, 13);
+            this.lblMorsePitch.TabIndex = 11;
+            this.lblMorsePitch.Text = "Morse-Pitch";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblMorsePitch);
+            this.Controls.Add(this.lblMorseInterval);
+            this.Controls.Add(this.nudMorseInterval);
+            this.Controls.Add(this.nudMorsePitch);
+            this.Controls.Add(this.rbnMorseToWordHearing);
+            this.Controls.Add(this.rbnMorseToLetterHearing);
+            this.Controls.Add(this.rbnMorseToLetter);
+            this.Controls.Add(this.rbnLetterToMorse);
+            this.Controls.Add(this.lblStreak);
+            this.Controls.Add(this.lblHint);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblNextLetter);
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudMorsePitch)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMorseInterval)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNextLetter;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label lblHint;
+        private System.Windows.Forms.Label lblStreak;
+        private System.Windows.Forms.RadioButton rbnLetterToMorse;
+        private System.Windows.Forms.RadioButton rbnMorseToLetter;
+        private System.Windows.Forms.RadioButton rbnMorseToLetterHearing;
+        private System.Windows.Forms.RadioButton rbnMorseToWordHearing;
+        private System.Windows.Forms.NumericUpDown nudMorsePitch;
+        private System.Windows.Forms.NumericUpDown nudMorseInterval;
+        private System.Windows.Forms.Label lblMorseInterval;
+        private System.Windows.Forms.Label lblMorsePitch;
+    }
+}
+
---
+++ MorseCodeTrainer/Form1.cs
@@ -0,0 +1,365 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace MorseCodeTrainer
+{
+	public partial class MainForm : Form
+	{
+		private List<LetterData> _listOfLetters;
+		private List<Keys> _listOfValidKeys;
+		private Random _random;
+		private System.Windows.Forms.Timer _timer;
+		private int __streak;
+		private int _streakSave;
+		private string[] _words;
+		private LearningMode _learningMode;
+		private bool _hearing = false;
+		private WordData _currentWord;
+		private LetterData _currentLetter;
+		private Thread beepThread;
+        private bool __cursorShown = true;
+
+		private int _streak
+		{
+			get { return __streak; }
+			set 
+			{
+				__streak = value;
+				lblStreak.Text = "Streak:" + Environment.NewLine + __streak;
+			}
+		}
+
+		private bool _cursorShown
+		{
+			get
+			{
+				return __cursorShown;
+			}
+			set
+			{
+				if (value == __cursorShown)
+				{
+					return;
+				}
+
+				if (value)
+				{
+					System.Windows.Forms.Cursor.Show();
+				}
+				else
+				{
+					System.Windows.Forms.Cursor.Hide();
+				}
+
+				__cursorShown = value;
+			}
+		}
+
+		private void MainForm_Load(object sender, EventArgs e)
+		{
+			txtInput.Left = (ClientSize.Width - txtInput.Size.Width) / 2;
+			lblStreak.Left = ClientSize.Width / 4 - txtInput.Size.Width;
+			_words = File.ReadAllText("words.txt").Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+
+            Click += MainForm_Click;
+            txtInput.KeyDown += txtInput_KeyDown;
+			txtInput.KeyUp += txtInput_KeyUp;
+            lblStreak.Click += lblStreak_Click;
+			rbnLetterToMorse.CheckedChanged += rbnSelectionChanged;
+            rbnMorseToLetter.CheckedChanged += rbnSelectionChanged;
+            rbnMorseToLetterHearing.CheckedChanged += rbnSelectionChanged;
+            rbnMorseToWordHearing.CheckedChanged += rbnSelectionChanged;
+			_timer.Tick += timer_Tick;
+			rbnLetterToMorse.Checked = true;
+        }
+
+		public MainForm()
+		{
+			_listOfLetters = new List<LetterData>();
+            _listOfValidKeys = new List<Keys>();
+            _random = new Random((int)(DateTime.Now.Ticks % int.MaxValue));
+			_timer = new System.Windows.Forms.Timer { Interval = 1 };
+			_timer.Start();
+
+
+            InitializeComponent();
+			FillLetters();
+			FillValidKeys();
+		}
+
+        private void MainForm_Click(object sender, EventArgs e)
+        {
+			_cursorShown = true;
+        }
+
+        private void rbnSelectionChanged(object sender, EventArgs e)
+        {
+            RadioButton rbnSender = (RadioButton)sender;
+            if (!rbnSender.Checked) return;
+            if (rbnLetterToMorse.Checked) _learningMode = LearningMode.TextFirstLetter;
+            if (rbnMorseToLetter.Checked || rbnMorseToLetterHearing.Checked) _learningMode = LearningMode.MorseFirstLetter;
+            if (rbnMorseToWordHearing.Checked) _learningMode = LearningMode.MorseFirstWord;
+            _hearing = rbnMorseToLetterHearing.Checked || rbnMorseToWordHearing.Checked;
+            SetNextSymbol();
+        }
+
+        private void txtInput_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Return) e.SuppressKeyPress = true;
+		}
+
+		private void txtInput_KeyUp(object sender, KeyEventArgs e)
+		{
+			if (!_listOfValidKeys.Contains(e.KeyCode)) { txtInput.Clear(); return; }
+
+            string correctAnswer = "";
+			string hint = "";
+            switch (_learningMode)
+            {
+                case LearningMode.TextFirstLetter:
+                    correctAnswer = _currentLetter.MorseCode;
+					hint = _currentLetter.GoogleHint;
+                    break;
+                case LearningMode.TextFirstWord:
+                    correctAnswer = _currentWord.MorseCode;
+                    break;
+                case LearningMode.MorseFirstLetter:
+                    correctAnswer = _currentLetter.Character;
+					hint = "Listen...";
+                    break;
+                case LearningMode.MorseFirstWord:
+                    correctAnswer = _currentWord.Word;
+					hint = "Listen...";
+                    break;
+            }
+			if (hint == "") hint = correctAnswer;
+
+			_cursorShown = false;
+			txtInput.Update();
+
+			if (e.KeyCode == Keys.Space)
+			{
+				txtInput.Clear();
+				if (lblHint.Text == "") {
+					lblHint.Text = hint;
+                    if (!_hearing) _streak = 0;
+                } else {
+					lblHint.Text = correctAnswer;
+                    _streak = 0;
+                }
+				if (_hearing) SoundMorse();
+                lblHint.Left = (ClientSize.Width - lblHint.Size.Width) / 2;
+			}
+			else if (e.KeyCode == Keys.Return || _learningMode == LearningMode.MorseFirstLetter)
+			{
+				lblHint.Text = "";
+				if (txtInput.Text.ToLower() == correctAnswer.ToLower())
+				{
+					txtInput.BackColor = Color.Green;
+					SetNextSymbol();
+					_streak++;
+				}
+				else
+				{
+					_streak = 0;
+					txtInput.BackColor = Color.Red;
+					if (_hearing) SoundMorse();
+                }
+				txtInput.Text = "";
+			}
+			else if ((_learningMode == LearningMode.TextFirstLetter || _learningMode == LearningMode.TextFirstWord)
+				!= (e.KeyCode == Keys.OemMinus || e.KeyCode == Keys.OemPeriod || e.KeyCode == Keys.Back))
+			{
+				txtInput.Clear();
+			}
+			e.Handled = true;
+			e.SuppressKeyPress = true;
+		}
+
+		private void SetNextSymbol()
+		{
+			switch (_learningMode)
+			{
+				case LearningMode.TextFirstLetter:
+				case LearningMode.MorseFirstLetter:
+                    _currentLetter = _listOfLetters[_random.Next(_listOfLetters.Count)];;
+                    break;
+				case LearningMode.TextFirstWord:
+				case LearningMode.MorseFirstWord:
+					string word = _words[_random.Next(_words.Length)];
+					_currentWord = new WordData(word, WordToMorse(word.RemoveSpecialCharacters()));
+					break;
+            }
+
+            switch (_learningMode)
+            {
+                case LearningMode.TextFirstLetter:
+                case LearningMode.TextFirstWord:
+					lblNextLetter.Text = _currentLetter.Character;
+					break;
+                case LearningMode.MorseFirstLetter:
+                    if (!_hearing) { lblNextLetter.Text = _currentLetter.MorseCode; } else { lblNextLetter.Text = ""; SoundMorse(); }
+                    break;
+                case LearningMode.MorseFirstWord:
+                    if (!_hearing) { lblNextLetter.Text = _currentWord.MorseCode; } else { lblNextLetter.Text = ""; SoundMorse(); }
+                    break;
+            }
+            lblNextLetter.Left = (ClientSize.Width - lblNextLetter.Size.Width) / 2;
+        }
+
+		private void timer_Tick(object sender, EventArgs e)
+		{
+			txtInput.BackColor = Color.FromArgb(Math.Min(txtInput.BackColor.R + 2, 255), Math.Min(txtInput.BackColor.G + 2, 255), Math.Min(txtInput.BackColor.B + 2, 255));
+		}
+
+		private void lblStreak_Click(object sender, EventArgs e)
+		{
+			_streak = _streakSave;
+			lblStreak.Text = "Streak:" + Environment.NewLine + _streak.ToString();
+		}
+
+		private void FillLetters()
+		{
+			_listOfLetters.Add(new LetterData("A", ".-", "Archery"));
+			_listOfLetters.Add(new LetterData("B", "-...", "Banjo"));
+			_listOfLetters.Add(new LetterData("C", "-.-.", "Candy"));
+			_listOfLetters.Add(new LetterData("D", "-..", "Dog"));
+			_listOfLetters.Add(new LetterData("E", ".", "Eye"));
+			_listOfLetters.Add(new LetterData("F", "..-.", "Firetruck"));
+			_listOfLetters.Add(new LetterData("G", "--.", "Giraffe"));
+			_listOfLetters.Add(new LetterData("H", "....", "Hippo"));
+			_listOfLetters.Add(new LetterData("I", "..", "Insect"));
+			_listOfLetters.Add(new LetterData("J", ".---", "Jet"));
+			_listOfLetters.Add(new LetterData("K", "-.-", "Kite"));
+			_listOfLetters.Add(new LetterData("L", ".-..", "Lab"));
+			_listOfLetters.Add(new LetterData("M", "--", "Mustache"));
+			_listOfLetters.Add(new LetterData("N", "-.", "Net"));
+			_listOfLetters.Add(new LetterData("O", "---", "Orchestra"));
+			_listOfLetters.Add(new LetterData("P", ".--.", "Paddles"));
+			_listOfLetters.Add(new LetterData("Q", "--.-", "Quarterback"));
+			_listOfLetters.Add(new LetterData("R", ".-.", "Robot"));
+			_listOfLetters.Add(new LetterData("S", "...", "Submarine"));
+			_listOfLetters.Add(new LetterData("T", "-", "Tape"));
+			_listOfLetters.Add(new LetterData("U", "..-", "Unicorn"));
+			_listOfLetters.Add(new LetterData("V", "...-", "Vacuum"));
+			_listOfLetters.Add(new LetterData("W", ".--", "Wand"));
+			_listOfLetters.Add(new LetterData("X", "-..-", "X-Ray"));
+			_listOfLetters.Add(new LetterData("Y", "-.--", "Yard"));
+			_listOfLetters.Add(new LetterData("Z", "--..", "Zebra"));
+		}
+
+		private void FillValidKeys()
+		{
+			foreach (Keys key in Enum.GetValues(typeof(Keys)))
+			{
+				int keyIndex = (int)key;
+				if (keyIndex == 8 || keyIndex == 13 || keyIndex == 32 || (keyIndex >= 65 && keyIndex <= 90) || keyIndex == 189 || keyIndex == 190)
+				{
+					_listOfValidKeys.Add(key);
+				}
+			}
+        }
+
+		private string WordToMorse(string word)
+		{
+			string morseString = "";
+			foreach (char c in word)
+			{
+				LetterData letter = _listOfLetters.Where(x => x.Character.ToLower() == c.ToString().ToLower()).First();
+				morseString += letter.MorseCode + " ";
+			}
+			return morseString.Trim();
+		}
+
+		private void SoundMorse()
+		{
+			string morse = "";
+            switch (_learningMode)
+            {
+                case LearningMode.MorseFirstLetter:
+                    morse = _currentLetter.MorseCode;
+                    break;
+                case LearningMode.MorseFirstWord:
+                    morse = _currentWord.MorseCode;
+                    break;
+            }
+
+            beepThread?.Abort();
+
+			Func<string, int, int, int> soundMorseLambda = (string pMorse, int pMorseInterval, int pMorsePitch) =>
+			{
+                foreach (char morseSign in pMorse)
+                {
+                    if (morseSign == '-') Console.Beep(pMorsePitch, pMorseInterval * 3);
+                    if (morseSign == '.') Console.Beep(pMorsePitch, pMorseInterval);
+                    if (morseSign == ' ') System.Threading.Thread.Sleep(pMorseInterval);
+                    System.Threading.Thread.Sleep(pMorseInterval);
+                }
+                return 1;
+            };
+            beepThread = new Thread(() => soundMorseLambda(morse, (int)nudMorseInterval.Value, (int)nudMorsePitch.Value));
+			
+            FormClosing += (object sender, FormClosingEventArgs e) => beepThread.Abort();
+            beepThread.Start();
+        }
+	}
+
+	public static class StringExtensions
+	{
+        public static string RemoveSpecialCharacters(this string str)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in str)
+            {
+                if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+                {
+                    sb.Append(c);
+                }
+            }
+            return sb.ToString();
+        }
+    }
+
+    public class LetterData
+	{
+		public string Character;
+		public string MorseCode;
+		public string GoogleHint;
+
+		public LetterData(string pCharacter, string pInMorseCode, string pGoogleHint)
+		{
+			Character = pCharacter;
+			MorseCode = pInMorseCode;
+			GoogleHint = pGoogleHint;
+		}
+	}
+
+    public class WordData
+    {
+        public string Word;
+        public string MorseCode;
+
+        public WordData(string pWord, string pMorse)
+        {
+            Word = pWord;
+			MorseCode = pMorse;
+        }
+    }
+
+    public enum LearningMode
+	{
+		TextFirstLetter,
+        TextFirstWord,
+        MorseFirstLetter,
+        MorseFirstWord
+	}
+}
